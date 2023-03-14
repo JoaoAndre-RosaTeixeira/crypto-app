b754754138207f88b7343c7049415b88aed6f704
JoaoAndre-RosaTeixeira <91742556+JoaoAndre-RosaTeixeira@users.noreply.github.com>
2023-03-14 11:07:37 +0100

    add test for blowwfish

---
+++ crypto_app/test_blowfish_algo.py
@@ -0,0 +1,55 @@
+import pytest
+from blowfish_algo import Blowfish
+
+class TestBlowfish:
+
+    def test_encrypt_decrypt(self):
+        bf = Blowfish()
+        message = "This is a secret message"
+        key = "My secret key"
+        encrypted_message = bf.encrypt(message, key)
+        decrypted_message = bf.decrypt(encrypted_message, key)
+        assert message == decrypted_message
+
+    def test_invalid_message_type(self):
+        bf = Blowfish()
+        message = 1234
+        key = "My secret key"
+        assert bf.encrypt(message, key) == False
+        assert bf.decrypt(message, key) == False
+
+    def test_invalid_key(self):
+        bf = Blowfish()
+        message = "This is a secret message"
+        key = "short"
+        assert bf.encrypt(message, key) == False
+        assert bf.decrypt(message, key) == False
+
+        key = "a" * 57
+        assert bf.encrypt(message, key) == False
+        assert bf.decrypt(message, key) == False
+
+        key = 1234
+        assert bf.encrypt(message, key) == False
+        assert bf.decrypt(message, key) == False
+
+    def test_invalid_encrypted_message(self):
+        bf = Blowfish()
+        message = "This is a secret message"
+        key = "My secret key"
+        encrypted_message = bf.encrypt(message, key)
+
+        # modify the encrypted message to make it invalid (change the last character)
+        invalid_message = encrypted_message[:-1] + 'x'
+
+        assert bf.decrypt(invalid_message, key) == 1
+
+    def test_wrong_key(self):
+        bf = Blowfish()
+        message = "This is a secret message"
+        key = "My secret key"
+        encrypted_message = bf.encrypt(message, key)
+
+        # try to decrypt with the wrong key
+        wrong_key = "My wrong key"
+        assert bf.decrypt(encrypted_message, wrong_key) == 0
